e07e19fd0d7ce5463d7d51df14a239f9cf99550f
lvichi <leonardovichi@gmail.com>
2023-12-30 21:54:24 +0000

    .

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,11 +6,104 @@
 /*   By: lvichi <lvichi@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 16:39:01 by lvichi            #+#    #+#             */
-/*   Updated: 2023/12/28 16:39:51 by lvichi           ###   ########.fr       */
+/*   Updated: 2023/12/30 21:50:55 by lvichi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char **argc)
-{
+#include "push_swap.h"
 
+static int	check_input(int len, char **array)
+{
+	ssize_t	i;
+	ssize_t	j;
+
+	i = -1;
+	while (++i < len)
+	{
+		j = -1;
+		if (array[i][0] == '-' || array[i][0] == '+')
+			j++;
+		while (array[i][++j])
+			if (array[i][j] < '0' || array[i][j] > '9')
+				return (0);
+		if (j > 11)
+			return (0);
+	}
+	return (1);
+}
+
+static int	check_duplicate(long n, long *numbers)
+{
+	ssize_t	i;
+	size_t	count;
+
+	count = 0;
+	i = 0;
+	while (++i <= numbers[0])
+		if (numbers[i] == n)
+			count++;
+	if (count != 1)
+		return (1);
+	return (0);
+}
+
+static long	*ft_split_array(int len, char **array)
+{
+	long	*numbers;
+	ssize_t	i;
+
+	if (!check_input(len, array))
+		return (NULL);
+	numbers = (long *)ft_calloc(sizeof(long), len + 1);
+	if (!numbers)
+		return (NULL);
+	numbers[0] = len;
+	i = -1;
+	while (array[++i])
+		numbers[i + 1] = ft_atoi(array[i]);
+	i = 0;
+	while (++i <= numbers[0])
+	{
+		if (numbers[i] > INT_MAX || numbers[i] < INT_MIN
+			|| check_duplicate(numbers[i], numbers))
+		{
+			free(numbers);
+			return (NULL);
+		}
+	}
+	return (numbers);
+}
+
+static long	*ft_nbr_split(char *str)
+{
+	long	*numbers;
+	ssize_t	i;
+	char	**array;
+
+	array = ft_split(str, ' ');
+	if (!array)
+		return (NULL);
+	numbers = ft_split_array(array_len(array), array);
+	i = -1;
+	while (array[++i])
+		free(array[i]);
+	free(array);
+	return (numbers);
+}
+
+int	main(int argc, char **argv)
+{
+	long	*numbers;
+
+	numbers = NULL;
+	if (argc < 2)
+		exit (0);
+	else if (argc == 2)
+		numbers = ft_nbr_split(argv[1]);
+	else
+		numbers = ft_split_array(argc - 1, &argv[1]);
+	if (!numbers)
+		write(1, "Error\n", 6);
+	else
+		init_stacks(numbers);
 }
---
+++ src/push_swap.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lvichi <lvichi@student.42porto.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/30 17:07:58 by lvichi            #+#    #+#             */
+/*   Updated: 2023/12/30 21:39:31 by lvichi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h> //delete
+# include <stdlib.h>
+# include <unistd.h>
+
+# define INT_MAX 2147483647
+# define INT_MIN -2147483648
+
+char	**ft_split(char const *s, char c);
+void	*ft_calloc(size_t nmemb, size_t size);
+size_t	array_len(char **array);
+size_t	str_len(char *str);
+long	ft_atoi(char *nptr);
+void	init_stacks(long *numbers);
+
+#endif
